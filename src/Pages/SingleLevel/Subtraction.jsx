e988907d5b21cbabc6a03ee7577f6ce70736389b
DostonUmarov <49802565+DostonUmarov@users.noreply.github.com>
2025-05-03 00:05:19 +0500

    Update Subtraction.jsx

--- src/Pages/SingleLevel/Subtraction.jsx
+++ src/Pages/SingleLevel/Subtraction.jsx
@@ -53,7 +53,7 @@ function Subtraction() {
     
   }
   function leaveGameConfirmation(){
-      const leaveGame = confirm("Are you sure you want to leave the game?")
+      const leaveGame = confirm("O'yinni tark etmoqchimisiz?")
       leaveGame ? navigate ("/category") : ""
   }
   function highScoreSetter(currentScore, category){
@@ -140,7 +140,7 @@ function Subtraction() {
         
 
         <p className="progress">
-          Question {currentQuestion} of 5
+          Savollar {currentQuestion} / 5
         </p>
 
         <p className="question">
@@ -154,21 +154,21 @@ function Subtraction() {
         <button 
         onClick={()=>{handleButtonClick()}}
         className={activeButton != null | activeButton != undefined? "" : "disabled"}
-        >Next Question</button>
+        >Keyingi savol</button>
 
       <div 
         ref={floatingScoreRef}        
         className="overlay">
           <div
         className="score-showcase">
-          <p>Oops, that's not the right answer, {`${randomNumber} ${sign} ${randomNumber2}`} is actually<span> {correctAnswer}</span></p>
+          <p>Afsus, Siz xato javobni tanladingiz, {`${randomNumber} ${sign} ${randomNumber2}`} javobi <span> {correctAnswer}</span> bo'ladi</p>
 
           <button
           onClick={()=>{
             floatingScoreRef.current.classList.remove("visible")
             nextGame()
           }}
-          >Continue</button>
+          >Davom etish</button>
         </div>
           </div>
     </main>
